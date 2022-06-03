3c7d26adf9ed9c1b5290b280790e5d2e5759e6ca
Mélanie <melanie.thomas.design@gmail.com>
2022-06-03 17:24:51 +0200

    green zone css ok

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
-console.log("hello");
-$(document).ready(function() {
-    console.log("hello2");
-    $(".image_accueil").hide();
+// console.log("hello");
+// $(document).ready(function() {
+//     console.log("hello2");
+//     $(".image_accueil").hide();
 
 
-});
+// });
